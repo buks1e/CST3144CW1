0e83c3e7e832f687a5e9b996c6a857d7ea532385
Ibukun <ibukundurodola@gmail.com>
2024-10-29 15:41:52 +0400

    Front End polished

---
+++ clubs.js
@@ -0,0 +1,73 @@
+let clubs = [
+    {
+        id:1,
+        subject: 'Music Club',
+        location: "Music Room",
+        price: 1500,
+        availableSlots: 10,
+        rating: 5,
+        image: "images/music_icon.png",
+        alt: 'Music Club Icon'
+    },
+    {
+        id:2,
+        subject: 'Swimming Club',
+        location: "The School Pool",
+        price: 1000,
+        availableSlots: 10,
+        rating: 3,
+        image: "images/swimming_icon.png",
+        alt: 'Swimming Club Icon'
+    },
+    {
+        id:3,
+        subject: 'Creative Writing Club',
+        location: "Classrooms",
+        price: 500,
+        availableSlots: 10,
+        rating: 2,
+        image: "images/writing_icon.png",
+        alt: 'Writing Club Icon'
+    },
+    {
+        id:4,
+        subject: 'Arts Club',
+        location: "Arts & Crafts Room",
+        price: 500,
+        availableSlots: 10,
+        rating: 3,
+        image: "images/arts_icon.png",
+        alt: 'Arts Club Icon'
+    },
+    {
+        id:5,
+        subject: 'WoodWorking Club',
+        location: "Woodshop Lab",
+        price: 1000,
+        availableSlots: 10,
+        rating: 2,
+        image: "images/woodworks_icon.png",
+        alt: 'Woodwork Club Icon'
+    },
+    {
+        id:6,
+        subject: 'Karate Club',
+        location: "Dance Room",
+        price: 1500,
+        availableSlots: 10,
+        rating: 2,
+        image: "images/karate_icon.png",
+        alt: 'Karate Club Icon'
+    },
+    {
+        id:7,
+        subject: 'Dance Club',
+        location: "Dance Room",
+        price: 500,
+        availableSlots: 10,
+        rating: 5,
+        image: "images/dance_icon.png",
+        alt: 'Dance Club Icon'
+    },
+
+]
